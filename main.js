97978b7f24a123d7220419af51c87e237e3c7204
Jiahao <jahowchen@outlook.com>
2020-01-06 14:30:35 -0500

    see if this works

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 //TO-DO: custom font, custom connector height, custom node gap,custorm margin
 //TO-DO: export to json
-import '/modules/draw.js';
+import './modules/draw.js';
 import { renderTarget, 
     renderSVG,
     genId,
     exportToSVG,
     exportToPNG,
-    clearEverything} from '/modules/draw.js';
+    clearEverything} from './modules/draw.js';
 let newSize = genId(renderTarget);
 renderSVG(renderTarget, newSize);
 
